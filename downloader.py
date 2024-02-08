41f622ac14097616303cc73c2711dc9689ef9a00
Hansana Dasanayaka <hansana123dasanayake@gmail.com>
2024-02-08 10:03:05 +0530

    Recreating the V2 With PyTube

--- downloader.py
+++ downloader.py
@@ -1,49 +1,29 @@
-from yt_dlp import YoutubeDL
+from pytube import YouTube
 
 
 print(f" ==========================\n Python YouTube Downloader \n​==========================\n")
 
-videoURL = str(input("Enter Video Link : "))
-# videoURL = 'https://www.youtube.com/watch?v=5cCaU6T1yFE'
+# videoURL = str(input("Enter Video Link : "))
+videoURL = 'https://www.youtube.com/watch?v=5cCaU6T1yFE'
+    
+yt = YouTube(videoURL)
+print(yt.title)
 
-# Progress bar shows when a video is downloading
-def download_loading_bar(d):
-    if d['status'] == 'downloading':
-        # Get the download percentage as a float
-        percentage = float(d['_percent_str'].replace('%', ''))
-        # Calculate how many # symbols to print
-        num_symbols = int(percentage / 5)
-        # Print the loading bar with the percentage and ETA, using \r and end=''
-        print(f"\rLoading: [{'#' * num_symbols}{'-' * (20 - num_symbols)}] {d['_percent_str']} {d['_eta_str']}", end='')
+# print("Fetching the info...")
 
-        
-ydl_opts = {
-    "quiet": True,
-    'outtmpl': "%(title)s_%(id)s.%(ext)s",
-    # "format": "bestvideo+bestaudio/best",
+    # vidTitle = 
+    # vidDuration = 
+    # vidSize = 
 
-    'progress_hooks': [download_loading_bar],
-    'no_color': True,
-}
+    # print("\n= Title:", vidTitle)
+    # print("= Duration:", vidDuration)
+    # print("= Size:", vidSize , "MB")
 
-print("Fetching the info...")
+# print("\nDownloading...")
 
-with YoutubeDL( ydl_opts ) as ydl:
-    vidInfo = ydl.extract_info(videoURL, download=False)
 
-    vidTitle = vidInfo.get('title', None)
-    vidDuration = vidInfo.get('duration', None)
-    vidSize = vidInfo.get('filesize_approx', None) // 1048576
 
-    print("\n🔹 Title:", vidTitle)
-    print("🔹 Duration:", vidDuration)
-    print("🔹 Size:", vidSize , "MB")
-
-print("\nDownloading...")
-
-ydl.download(['https://www.youtube.com/watch?v=BxUS1K7xu30'])
-
-print("Downloaded Successfully! \n ")
+# print("Downloaded Successfully! \n ")
 
 
     
