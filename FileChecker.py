ded83a12d6e3d547486d4308d71ca5d15e30e08d
Hansana Dasanayaka <hansana123dasanayake@gmail.com>
2024-02-08 21:57:22 +0530

    Removed unwanted libraries and files

--- FileChecker.py
+++
@@ -1,5 +0,0 @@
-import os
-print("\nCurrent Working Directory: ", os.getcwd())
-print(  os.path.isfile('vids\Gwa Gwa Gwa Sound Effect.mp3')  )
-
-print("\n", os.path.join(os.getcwd(), "vids","Gwa Gwa Gwa Sound Effect.mp3"))
