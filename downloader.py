fd3e10a455469fa88157479a8436a06357fd98d5
Hansana Dasanayaka <hansana123dasanayake@gmail.com>
2024-02-08 13:26:41 +0530

    Download Audio & Video Seperately.

--- downloader.py
+++ downloader.py
@@ -1,29 +1,39 @@
 from pytube import YouTube
+import pathlib
 
 
-print(f" ==========================\n Python YouTube Downloader \n​==========================\n")
+print(f"===============================\n Python YouTube Downloader v2.0\n===============================\n")
 
 # videoURL = str(input("Enter Video Link : "))
-videoURL = 'https://www.youtube.com/watch?v=5cCaU6T1yFE'
-    
+videoURL = 'https://www.youtube.com/watch?v=mDTMBdYAjHI'
+# https://youtu.be/krsBRQbOPQ4?si=UKY-g36vKVM7Umqc : MrBeast Video
+
 yt = YouTube(videoURL)
-print(yt.title)
-
-# print("Fetching the info...")
-
-    # vidTitle = 
-    # vidDuration = 
-    # vidSize = 
-
-    # print("\n= Title:", vidTitle)
-    # print("= Duration:", vidDuration)
-    # print("= Size:", vidSize , "MB")
-
-# print("\nDownloading...")
 
 
+print("-------VIDEOS-------")
 
-# print("Downloaded Successfully! \n ")
+for count, stream in enumerate(yt.streams.filter(only_video=True, mime_type="video/mp4"), start=1):
+    print(f"{count})  Res: {stream.resolution}  |  Size:{stream.filesize_mb} mb")
+    # print(stream)
+
+userInput = input(str("Enter Res: "))
+
+for stream in yt.streams.filter(only_video=True, mime_type="video/mp4", res=userInput):
+    stream.download(filename=f"{yt.title}.mp4", output_path=f"{pathlib.Path(__file__).parent.resolve()}/vids")
+    print(userInput, ": MP4 Downloaded.✔")
+
+
+
+print("-------AUDIOS-------")
+
+for stream in yt.streams.filter(only_audio=True, abr="128kbps"):
+    stream.download(filename=f"{yt.title}.mp3", output_path=f"{pathlib.Path(__file__).parent.resolve()}/vids")
+    print(stream.abr,": MP3 Downloaded. ✔")
+
+    
+      
+
 
 
     
